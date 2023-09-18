3a94d1f5fea4937ea6a4b4aa74553d8144a5a960
FranChiz28 <fcssalvador28@gmail.com>
2023-09-18 17:09:48 +0800

    Francis
    edited row gap for reservation

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res)=>{
-    res.render("movieViews", {title: "Movie Info", user:"Name", userRevenue:"Php 10,000.000", allRevenue:"Php 100,000.00"});
+    res.render("movieViews", {title: "Movie Info"});
 });
 
 
