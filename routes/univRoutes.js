6feffee9188615f8fc7cd0cbd5b2813a685136c4
FranChiz28 <fcssalvador28@gmail.com>
2023-09-22 20:53:47 +0800

    Francis
    trying to deploy

--- routes/univRoutes.js
+++ routes/univRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const axios = require('Axios');
+const axios = require('axios');
 const collection = require("../module/mongodb");
 
 const user1 = {
